56ea73fcd7cd8603873908cec8d3d318bab5d5f1
Robert Kuznetsov <robot3@xaker.ru>
2016-06-03 09:52:46 +0300

    Embed PHP within project. Add define routes.

---
+++ header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Официальный сайт СаноГенного Мышления (СГМ) профессора Ю.М. Орлова. Саногенное и патогенное мышление (теория и практика, ответы на вопросы). Информация о всех книгах и брошюрах Ю.М.Орлова.</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+
+
+            <div class="jumbotron">
+                <p>
+                    <div class="thumbnail">
+                        <img src="img/pictop.jpg" alt="" />
+                    </div>
+                </p>
+                <p class="top-thesis">СаноГенное Мышление (СГМ) — научная философия повседневной жизни человека XXI века.</p>
+            </div>
+
+            <!-- Static navbar -->
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div id="navbar" class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="index.html">Главная</a></li>
+                            <li><a href="news.html">Рекомендуем</a></li>
+                            <li><a href="books.html">Книги</a></li>
+                            <li><a href="videos.html">Видео</a></li>
+                            <li><a href="lexio.html">Лекции</a></li>
+                            <li><a href="publications.html">Публикации</a></li>
+                            <li><a href="KursSGM.html">Занятия</a></li>
+                            <li><a href="narodovlastie.html">СГМ и общество</a></li>
+                        </ul>                        
+                    </div><!--/.nav-collapse -->
+                </div><!--/.container-fluid -->
+            </nav>
+            
+            <div class="content">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="panel panel-warning">
+                            <div class="panel-heading">Рекомендуем</div>
+                            <div class="panel-body">
+                                <p>В разделе <a href="publications.html"><em>«Публикации»</em></a> очень интересное интервью Юрия Михайловича Орлова от 2 марта 1999 года. Название: <a href="publications-intreview-ot-2-mart-1999g.html"><strong>«Саногенное мышление — философия XXI века»</strong></a>. — Москва, 1999.</p>
+                            </div>
+                        </div>
+                        <div class="panel panel-default">
+                            <div class="panel-body link-block">
+                                <p><strong>Ответы на вопросы</strong></p>
+                                <p><span class="glyphicon glyphicon-envelope"></span> <strong>E-mail sanogen@mail.ru</strong></p>
+                                <p><strong>СГМ в Интернете</strong></p>
+                                <p><strong>Форум СГМ</strong></p>
+                            </div>
+                        </div>
+                        <div class="panel panel-warning">
+                            <div class="panel-body">
+                                <p>В разделе <em>«Главная»</em> статью из психологической газеты "Мы и Мир": <strong>"Саногенное мышление — философия радости и благополучия"</strong>. — Москва, 2003.</p>
+                                <p>В разделе <em>«Публикации»</em> брошюру <strong>"Беседа о саногенном мышлении"</strong>. — Москва, 1999.</p>
+                                <p>В разделе <em>«Главная»</em> <strong>"Более полная биография Ю.М. Орлова"</strong>. — Москва, 1998.</p>
+                                <p>В разделе <em>«CГМ и общество»</em> брошюру Ю.М. Орлова <strong>"Структура прямого народовластия"</strong>. — Москва, 1996.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-9">
